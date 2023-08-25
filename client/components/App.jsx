c23877b21a06700e0db169801e2b36ead5e65d41
Aalayah-Olaes <108641773+AalayahOlaes@users.noreply.github.com>
2023-08-25 15:25:49 -0400

    buttons with same state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import '../style.css';
+import { useState } from 'react';
+
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  function MyButton({ count, onClick }) {
+    return (
+      <button onClick={onClick}>
+        Clicked {count} times
+      </button>
+    );
+  }
+
   return (
     <div>
-      <h1>Hello world!</h1>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick}></MyButton>
+      <MyButton count={count} onClick={handleClick}></MyButton>
     </div>
   );
 };
